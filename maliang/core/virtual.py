5ec8988b0cb438b80d8fb37b6ca0e17aed77ff45
Zhikang Yan <2951256653@qq.com>
2025-06-06 22:20:56 +0800

    feat(virtual): add method `lift` to class `virtual.Widget` (#116)

--- maliang/core/virtual.py
+++ maliang/core/virtual.py
@@ -1002,6 +1002,16 @@ class Widget:
         for element in self.elements:
             element.forget(value)
 
+    def lift(self) -> None:
+        """Lift the widget to the top."""
+        self.master.widgets.remove(self)
+        self.master.widgets.append(self)
+        for element in self.elements:
+            for item in tuple(element.items):
+                self.master.tag_raise(item)
+        for widget in self.children:
+            widget.lift()
+
     def move(self, dx: float, dy: float) -> None:
         """Move the widget.
 
